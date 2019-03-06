42f32ea1874335dc6bf2e86859b0cd5a8e19ae11
dongaiiqng <dongaiiqng@gmail.com>
2019-03-06 12:20:17 -0600

    Added home page

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,25 +1,28 @@
 import React, { Component } from 'react'
-import { Button } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 
 // 'Home' is already defined as the class name, so provide an alias
 // 'Home-header' from the SCSS file gets transformed to 'HomeHeader'
 //    because '-' is not allowed in JS variable names
-import { Home as HomeCss, HomeHeader } from './Home.module.scss'
+import { Home as HomeCss, HomeContents, HomeButton, HomeGradient, HomeHeader} from './Home.module.scss'
 
 class Home extends Component {
   render() {
     return (
       <div className={HomeCss}>
-        <h1 className={HomeHeader}>Welcome to the MP2 React Demo!</h1>
+        <div className={HomeContents}>
+          <div className={HomeHeader}>
+          <p>Welcome to</p>
+          <h1>Movie Gallery</h1>
+          </div>
 
-        <p>This a landing page for demoing routing.</p>
-
-        <Link to="/pokemon">
-          <Button>
-            Click here to go to the demo
-          </Button>
-        </Link>
+          <Link to="/movies">
+            <div className={HomeButton}>
+              Explore
+            </div>
+          </Link>
+        </div>
+        <div className={HomeGradient}></div>
       </div>
     )
   }
